2f72c80ac4d69b9c70caea4302af4319b2f48887
duping <553240007@qq.com>
2020-05-20 15:33:13 +0800

    添加回调函数，打印

--- demo/datastuct/binarysearchtree/binarysearchtree.go
+++ demo/datastuct/binarysearchtree/binarysearchtree.go
@@ -1,9 +1,18 @@
 package binarysearchtree
 
+import (
+	"container/list"
+	"fmt"
+)
+
 type BinarySearchTree struct {
-	nodeRoot   *Node
-	size       int
+	nodeRoot  *Node
+	size      int
+	VCallBack CB
+	IsStop    bool //是否停止遍历
 }
+
+type CB func(v interface{}) bool
 
 type Node struct {
 	Element    interface{}
@@ -77,4 +86,91 @@ func (t *BinarySearchTree) checkElementNotNil(e interface{}) {
 	if e == nil {
 		panic("element not nil")
 	}
+}
+
+//前序遍历
+
+func (t *BinarySearchTree) PreOrderPrint() {
+	t.PreOrderPrintLooper(t.nodeRoot)
+}
+
+func (t *BinarySearchTree) PreOrderPrintLooper(n *Node) {
+
+	if n == nil || t.IsStop {
+		return
+	}
+
+	if t.VCallBack != nil {
+		t.IsStop = t.VCallBack(n.Element)
+	}
+	//fmt.Println(n.Element)
+	t.PreOrderPrintLooper(n.NodeLeft)
+	t.PreOrderPrintLooper(n.NodeRight)
+}
+
+//中序遍历
+
+func (t *BinarySearchTree) MidOrderPrint() {
+	t.MidOrderPrintLooper(t.nodeRoot)
+}
+
+func (t *BinarySearchTree) MidOrderPrintLooper(n *Node) {
+
+	if n == nil || t.IsStop {
+		return
+	}
+
+	t.MidOrderPrintLooper(n.NodeLeft)
+
+	if t.IsStop {
+		return
+	}
+
+	if t.VCallBack != nil {
+		t.IsStop = t.VCallBack(n.Element)
+	}
+	//fmt.Println(n.Element)
+	t.MidOrderPrintLooper(n.NodeRight)
+}
+
+//后序遍历
+
+func (t *BinarySearchTree) AfterOrderPrint() {
+	t.AfterOrderPrintLooper(t.nodeRoot)
+}
+
+func (t *BinarySearchTree) AfterOrderPrintLooper(n *Node) {
+
+	if n == nil {
+		return
+	}
+
+	t.AfterOrderPrintLooper(n.NodeLeft)
+	t.AfterOrderPrintLooper(n.NodeRight)
+	fmt.Println(n.Element)
+}
+
+//层序遍历
+func (t *BinarySearchTree) LevelOrderPrint() {
+	if t.nodeRoot == nil {
+		return
+	}
+
+	l := list.New()
+	l.PushBack(t.nodeRoot)
+
+	for l.Len() != 0 {
+		n := l.Remove(l.Front()).(*Node)
+		if t.VCallBack != nil {
+			_ = t.VCallBack(n.Element)
+		}
+		//fmt.Println(n.Element)
+		if n.NodeLeft != nil {
+			l.PushBack(n.NodeLeft)
+		}
+		if n.NodeRight != nil {
+			l.PushBack(n.NodeRight)
+		}
+	}
+
 }
--- demo/datastuct/main.go
+++ demo/datastuct/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"github.com/dpCnx/go-study/demo/datastuct/binarysearchtree"
+	"log"
 )
 
 func main() {
-	var a binarysearchtree.Myint = 1
-	var b binarysearchtree.Myint = 2
+	var a binarysearchtree.Myint = 9
+	var b binarysearchtree.Myint = 3
+	var c binarysearchtree.Myint = 7
+	var d binarysearchtree.Myint = 5
+	var e binarysearchtree.Myint = 8
+	var f binarysearchtree.Myint = 15
+	var g binarysearchtree.Myint = 13
+	var h binarysearchtree.Myint = 14
+	var i binarysearchtree.Myint = 4
 
 	t := binarysearchtree.BinarySearchTree{}
 	t.Add(a)
 	t.Add(b)
+	t.Add(c)
+	t.Add(d)
+	t.Add(e)
+	t.Add(f)
+	t.Add(g)
+	t.Add(h)
+	t.Add(i)
 
-	fmt.Println(t.GetSize())
+	y := 0
+	t.VCallBack = func(v interface{}) bool {
+		if y == 2 {
+			return true
+		}
 
+		log.Println(v)
+		y++
+		return false
+	}
+
+	t.PreOrderPrint()
 }
